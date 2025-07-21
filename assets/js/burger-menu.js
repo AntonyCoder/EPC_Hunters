ed154845cdd86462e577de566a270bb131b62e1d
Anton Lysenko <antonlysenko161@yandex.ru>
2025-07-21 11:41:24 +0300

    Update script

--- assets/js/burger-menu.js
+++ assets/js/burger-menu.js
@@ -3,16 +3,16 @@ const headerMenu = document.querySelector('.header-menu');
 const burgerCloseButton = document.querySelector('.burger-close-button');
 const menuOverlay = document.querySelector('.menu-overlay');
 
-burgerButton.addEventListener('click', () => showMenu());
-burgerCloseButton.addEventListener('click', () => hideMenu());
-menuOverlay.addEventListener('click', () => hideMenu());
+burgerButton.addEventListener('click', showMenu);
+burgerCloseButton.addEventListener('click', hideMenu);
+menuOverlay.addEventListener('click', hideMenu);
 
-function showMenu(){
+function showMenu() {
     headerMenu.classList.remove('hidden-menu');
     menuOverlay.classList.remove('hidden');
 }
 
-function hideMenu(){
+function hideMenu() {
     headerMenu.classList.add('hidden-menu');
     menuOverlay.classList.add('hidden');
 }
